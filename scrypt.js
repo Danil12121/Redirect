58f96cc2b67cb777499bb897d9f7fe44d5e71426
Даниил <95086085+Danil12121@users.noreply.github.com>
2025-04-26 18:46:27 +0300

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -31,7 +31,7 @@
                 var start = Date.now();
                 
                 // Пробуем открыть приложение с зашифрованными данными
-                window.location = `bank://payment?data=${encodeURIComponent(encryptedData)}`;
+                window.location = `mybank://payment?data=${encodeURIComponent(encryptedData)}`;
                 
                 setTimeout(function() {
                     var end = Date.now();
